230fc327be23841b2143c093342ce3f285f9221d
Bogdan252525 <bogdan2516@gmail.com>
2024-10-01 11:47:22 +0300

    feat implement cli and cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,31 @@
+import { spawn } from 'child_process';
+import { stdin, stdout } from 'process';
+import path from 'path';
+
+const PATH_TO_SCRIPT = path.join('src', 'cp', 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  if (!Array.isArray(args)) {
+    args = [];
+  }
+
+  const child = spawn('node', [PATH_TO_SCRIPT, ...args], {
+    stdio: ['pipe', 'pipe', 'pipe'],
+  });
+
+  stdin.pipe(child.stdin);
+
+  child.stdout.pipe(stdout);
+
+  child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  child.on('close', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const args = process.argv.slice(2);
+
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);

8a1ee574808c31f33e5e1fe2fbc3300eb2ab4d25
Bogdan252525 <bogdan2516@gmail.com>
2024-10-01 16:12:03 +0300

    feat: implement fs and hash

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+const SOURCE_PATH = path.join('src', 'fs', 'files');
+const DEST_PATH = path.join('src', 'fs', 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.access(SOURCE_PATH);
+    await fs
+      .access(DEST_PATH)
+      .then(() => {
+        throw new Error('FS operation failed');
+      })
+      .catch((err) => {
+        if (err.code !== 'ENOENT') throw err;
+      });
+
+    await fs.cp(SOURCE_PATH, DEST_PATH, { recursive: true });
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+const WRONG_FILE_PATH = path.join('src', 'fs', 'files', 'wrongFilename.txt');
+const PROPER_FILE_PATH = path.join('src', 'fs', 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fs.access(WRONG_FILE_PATH);
+    await fs
+      .access(PROPER_FILE_PATH)
+      .then(() => {
+        throw new Error('FS operation failed');
+      })
+      .catch((err) => {
+        if (err.code !== 'ENOENT') throw err;
+      });
+
+    await fs.rename(WRONG_FILE_PATH, PROPER_FILE_PATH);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();

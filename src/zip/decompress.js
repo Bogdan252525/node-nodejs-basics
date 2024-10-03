6c40373846db9554c990ca41ac63adcc12ea327c
Bogdan252525 <bogdan2516@gmail.com>
2024-10-03 16:46:09 +0300

    feat: implement wt and zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { pipeline as pipe } from 'node:stream/promises';
+import path from 'node:path';
+
+const INPUT_PATH = path.join('src', 'zip', 'files', 'archive.gz');
+const OUTPUT_PATH = path.join('src', 'zip', 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+	const input = createReadStream(INPUT_PATH);
+  const output = createWriteStream(OUTPUT_PATH);
+  const gunzip = createGunzip();
+
+  await pipe(input, gunzip, output);
+
+  console.log('The file is successfully decompressed in fileToCompress.txt');
 };
 
 await decompress();
